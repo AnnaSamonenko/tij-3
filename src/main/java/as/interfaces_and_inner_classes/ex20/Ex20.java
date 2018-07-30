a21ed84fba4bb21c4c3e839a160098c2550f0bb8
Anna Samonenko <annsamonenko97@gmail.com>
2018-07-30 23:52:09 +0300

    ex20

--- src/main/java/as/interfaces_and_inner_classes/ex20/Ex20.java
+++ src/main/java/as/interfaces_and_inner_classes/ex20/Ex20.java
@@ -1,7 +1,10 @@
 package as.interfaces_and_inner_classes.ex20;
 
-/**
- * Create a class containing a nested class. In main( ), create an instance of the inner class.
- */
-public class Ex20 {
+class Ex20 {
+
+    static class StaticClass {
+        public void print() {
+            System.out.print("Hello World");
+        }
+    }
 }
---
+++ src/main/java/as/interfaces_and_inner_classes/ex20/Main.java
@@ -0,0 +1,13 @@
+package as.interfaces_and_inner_classes.ex20;
+
+/**
+ * Create a class containing a nested class. In main( ), create an instance of the inner class.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ex20.StaticClass staticClass = new Ex20.StaticClass();
+        staticClass.print();
+    }
+}
